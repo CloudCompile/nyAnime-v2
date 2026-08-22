bbb32c7a9622a164fa7c9a3c5cdc493087284ebb
CJ Hauser <christopher.j.hauser2025@outlook.com>
2026-08-22 13:55:33 -0500

    Use deployable serverless import paths

--- api/sources.ts
+++ api/sources.ts
@@ -1,4 +1,4 @@
-import { aniwavesService } from '../src/services/aniwavesService.ts';
+import { aniwavesService } from '../src/services/aniwavesService.js';
 
 export default async function handler(req: any, res: any) {
   const episodeId = String(req.query?.episodeId || '');
